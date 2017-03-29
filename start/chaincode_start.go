54ee946e0d87da53fa17dd1293553ee156c39e6a
Ted <teddychene@Teds-Mac.lan>
2017-03-29 09:56:08 -1000

    Compiled my code

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -57,7 +57,10 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 	// Handle different functions
 	if function == "init" {													//initialize the chaincode state, used as reset
 		return t.Init(stub, "init", args)
+	} else if function =="write"{
+		return t.write(stub, args)
 	}
+	
 	fmt.Println("invoke did not find func: " + function)					//error
 
 	return nil, errors.New("Received unknown function invocation: " + function)
@@ -76,3 +79,48 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 
 	return nil, errors.New("Received unknown function query: " + function)
 }
+
+// Function write 
+
+func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	var key, value string 
+	var err error 
+	
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the key and value and value to set")
+	}
+	key=args[0]
+	value=args[1]
+	err:stub.PutState(key, []byte(value))
+	if err != nil{
+		return nil, err
+	}
+	return nil, nil 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
